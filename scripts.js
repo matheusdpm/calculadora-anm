8cc0d5438f809a594733085adaabc7c12cffff92
matheusdpm <73431651+matheusdpm@users.noreply.github.com>
2020-12-20 14:48:12 -0300

    update code logic

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+/*
+	dataVencimento;
+	dataInicio=01/01/2021;
+	diasDeProrrogação=288;
+
+	ALVARÁ DE PESQUISA, GUIAS DE UTILIZAÇÃO, REGISTRO DE LICENÇA E PERMISSÃO DE LAVRA GARIMPEIRA:
+prorrogar=288- (dataVencimento-dataInicio);
+
+	TÍTULOS OUTORGADOS ENTRE 20 DE MARÇO DE 2020 E 1° DE JANEIRO DE 2021 SERÃO ACRESCIDOS DE MAIS ATÉ 288 DIAS, OBSERVANDO-SE O SEGUINTE CRITÉRIO: 
+prorrogar=min(288,dataPublicação-dataInicio);
+
+	TÍTULOS VENCIDOS EM 1° DE JANEIRO 
+prorrogar = 288;
+
+*/
+
+var caso = "case1";
+
 function setDate(id){
 	let today = new Date();
 	let month = ('0' + (today.getMonth() + 1)).slice(-2);
@@ -19,7 +37,35 @@ function getDataString(date){
 function updateResult(){
 	let div = document.getElementById('result');
 	let date = document.getElementById('data');
-	let data = new Date(date.value + "GMT-0300")
-	data.setMonth( data.getMonth() + 9 );
-	div.textContent = "A data para sua coisa é: " + getDataString(data);
+	let data = new Date(date.value + "GMT-0300");
+	let dataInicio=new Date("01/01/2021 GMT-0300");
+
+	var prorrogar = 288;
+	var diff = dataInicio.getTime() - data.getTime();   
+	var daydiff = (diff / 31536000000).toFixed(0);     
+	if (caso == "case3") {
+		prorrogar=288;
+	} else if (caso == "case2") {
+		prorrogar=min(288,prorrogar - daydiff);
+	} else {
+		prorrogar=288 - daydiff;
+	}
+	var strtDt  = new Date("2020-03-19");
+	var endDt  = new Date("2021-01-02");
+	if (data <= strtDt){
+		div.textContent = "Antes do Prazo estabelecido";
+	} else if (data >= endDt){
+		div.textContent = "Depois do prazo estabalecido";
+	} else {
+		data.setDate(data.getDate()+prorrogar)
+		div.textContent = "A data para sua coisa é: " + getDataString(data);
+	}
 }
+
+
+function updateRadio(id){
+	let a = document.getElementById(id);
+	caso=id;
+}
+
+updateResult();
